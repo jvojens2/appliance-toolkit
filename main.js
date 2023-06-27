a41acb6e25dbf01eee9d869605993d48cab23030
Julian Farley <jvojens2@gmail.com>
2023-06-26 23:47:43 -0500

    Add CDN jQuery and click event to Parse button

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-This is the main JS file for the appliance-toolkit repo.
+// This is the main JS file for the appliance-toolkit repo.
+
+
+
+
+
+function parseApplianceParts(partOne, partTwo) {
+	if ( partOne.length === 0 || partTwo.length === 0 ) { alert("Must enter both part numbers!"); }
+	else {
+		alert("Great job entering both part numbers!");
+	}
+}
+
+$( "#parse-btn" ).on( "click", function() {
+  // alert("Parse Button clicked!");
+  parseApplianceParts($("#appliance-part-one"), $("#appliance-part-two"));
+} );
